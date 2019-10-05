beeb176bdfc0578e2487a3ea93e110f608e1416d
Ryan Kotzen <ryan@krimzen.ninja>
2019-10-05 12:09:42 +0200

    Adding in code from other library

--- android/src/main/java/com/rnmmpowermanager/MultiManufacturerPowerManagerModule.java
+++ android/src/main/java/com/rnmmpowermanager/MultiManufacturerPowerManagerModule.java
@@ -4,6 +4,13 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeMap;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 
 public class MultiManufacturerPowerManagerModule extends ReactContextBaseJavaModule {
 
@@ -19,9 +26,47 @@ public class MultiManufacturerPowerManagerModule extends ReactContextBaseJavaMod
         return "MultiManufacturerPowerManager";
     }
 
+    private static final Intent[] POWERMANAGER_INTENTS = {
+      new Intent().setComponent(
+          new ComponentName("com.miui.securitycenter", "com.miui.permcenter.autostart.AutoStartManagementActivity")),
+      new Intent().setComponent(
+          new ComponentName("com.letv.android.letvsafe", "com.letv.android.letvsafe.AutobootManageActivity")),
+      new Intent().setComponent(
+          new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.process.ProtectActivity")),
+      new Intent().setComponent(new ComponentName("com.coloros.safecenter",
+          "com.coloros.safecenter.permission.startup.StartupAppListActivity")),
+      new Intent().setComponent(
+          new ComponentName("com.coloros.safecenter", "com.coloros.safecenter.startupapp.StartupAppListActivity")),
+      new Intent()
+          .setComponent(new ComponentName("com.oppo.safe", "com.oppo.safe.permission.startup.StartupAppListActivity")),
+      new Intent()
+          .setComponent(new ComponentName("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.AddWhiteListActivity")),
+      new Intent()
+          .setComponent(new ComponentName("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.BgStartUpManager")),
+      new Intent().setComponent(new ComponentName("com.vivo.permissionmanager",
+          "com.vivo.permissionmanager.activity.BgStartUpManagerActivity")),
+      new Intent().setComponent(new ComponentName("com.asus.mobilemanager", "com.asus.mobilemanager.MainActivity")) };
+
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void startIntent(String pkg, String cls) {
+        Intent intent = new Intent();
+        ComponentName componentName = new ComponentName(pkg, cls);
+        intent.setComponent(componentName);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        reactContext.startActivity(intent);
+    }
+
+    @ReactMethod
+    public void isHasProtectManager(final Promise promise) {
+        for (Intent intent : POWERMANAGER_INTENTS)
+        if (reactContext.getPackageManager().resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY) != null) {
+            WritableMap map = new WritableNativeMap();
+            map.putString("package", intent.getComponent().getPackageName());
+            map.putString("class", intent.getComponent().getClassName());
+            promise.resolve(map);
+            return;
+
+        }
+        promise.resolve(false);
     }
 }
