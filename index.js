31790b493cc28309b496ea7ee980b128813ccbdc
Ryan Kotzen <ryan@krimzen.ninja>
2019-10-05 11:44:48 +0200

    Adding in templated code

---
+++ index.js
@@ -0,0 +1,5 @@
+import { NativeModules } from 'react-native';
+
+const { MultiManufacturerPowerManager } = NativeModules;
+
+export default MultiManufacturerPowerManager;
---
+++ ios/MultiManufacturerPowerManager.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface MultiManufacturerPowerManager : NSObject <RCTBridgeModule>
+
+@end
