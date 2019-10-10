04c1915c7a8e48a3a0a7ce7023c178b79bb42b13
Ryan Kotzen <ryan@krimzen.ninja>
2019-10-10 09:02:47 +0200

    Adding ability to check if has intent separately.

--- android/src/main/java/com/rnmmpowermanager/MultiManufacturerPowerManagerModule.java
+++ android/src/main/java/com/rnmmpowermanager/MultiManufacturerPowerManagerModule.java
@@ -26,60 +26,75 @@ public class MultiManufacturerPowerManagerModule extends ReactContextBaseJavaMod
         return "MultiManufacturerPowerManager";
     }
 
-    /** List compiled from the following sources:
+    /**
+     * List compiled from the following sources:
      * https://stackoverflow.com/a/48641229/1248080
      * https://github.com/dirkam/backgroundable-android
      */
     private static final Intent[] POWERMANAGER_INTENTS = {
-      new Intent().setComponent(
-          new ComponentName("com.miui.securitycenter", "com.miui.permcenter.autostart.AutoStartManagementActivity")),
-      new Intent().setComponent(
-          new ComponentName("com.miui.securitycenter", "com.miui.powercenter.PowerSettings")),
-      new Intent().setComponent(
-          new ComponentName("com.letv.android.letvsafe", "com.letv.android.letvsafe.AutobootManageActivity")),
-      new Intent().setComponent(
-          new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.process.ProtectActivity")),
-      new Intent().setComponent(
-          new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.appcontrol.activity.StartupAppControlActivity")),
-      new Intent().setComponent(
-          new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.startupmgr.ui.StartupNormalAppListActivity")),
-      new Intent().setComponent(new ComponentName("com.coloros.safecenter",
-          "com.coloros.safecenter.permission.startup.StartupAppListActivity")),
-      new Intent().setComponent(
-          new ComponentName("com.coloros.safecenter", "com.coloros.safecenter.startupapp.StartupAppListActivity")),
-      new Intent()
-          .setComponent(new ComponentName("com.oppo.safe", "com.oppo.safe.permission.startup.StartupAppListActivity")),
-      new Intent()
-          .setComponent(new ComponentName("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.AddWhiteListActivity")),
-      new Intent()
-          .setComponent(new ComponentName("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.BgStartUpManager")),
-      new Intent().setComponent(new ComponentName("com.vivo.permissionmanager",
-          "com.vivo.permissionmanager.activity.BgStartUpManagerActivity")),
-      new Intent().setComponent(new ComponentName("com.samsung.android.lool",
-          "com.samsung.android.sm.ui.battery.BatteryActivity")),
-      new Intent().setComponent(new ComponentName("com.htc.pitroad",
-          "com.htc.pitroad.landingpage.activity.LandingPageActivity")),
-      new Intent().setComponent(new ComponentName("com.asus.mobilemanager", "com.asus.mobilemanager.MainActivity")) };
+            new Intent().setComponent(new ComponentName("com.miui.securitycenter",
+                    "com.miui.permcenter.autostart.AutoStartManagementActivity")),
+            new Intent()
+                    .setComponent(new ComponentName("com.miui.securitycenter", "com.miui.powercenter.PowerSettings")),
+            new Intent().setComponent(
+                    new ComponentName("com.letv.android.letvsafe", "com.letv.android.letvsafe.AutobootManageActivity")),
+            new Intent().setComponent(new ComponentName("com.huawei.systemmanager",
+                    "com.huawei.systemmanager.optimize.process.ProtectActivity")),
+            new Intent().setComponent(new ComponentName("com.huawei.systemmanager",
+                    "com.huawei.systemmanager.appcontrol.activity.StartupAppControlActivity")),
+            new Intent().setComponent(new ComponentName("com.huawei.systemmanager",
+                    "com.huawei.systemmanager.startupmgr.ui.StartupNormalAppListActivity")),
+            new Intent().setComponent(new ComponentName("com.coloros.safecenter",
+                    "com.coloros.safecenter.permission.startup.StartupAppListActivity")),
+            new Intent().setComponent(new ComponentName("com.coloros.safecenter",
+                    "com.coloros.safecenter.startupapp.StartupAppListActivity")),
+            new Intent().setComponent(
+                    new ComponentName("com.oppo.safe", "com.oppo.safe.permission.startup.StartupAppListActivity")),
+            new Intent().setComponent(
+                    new ComponentName("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.AddWhiteListActivity")),
+            new Intent().setComponent(
+                    new ComponentName("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.BgStartUpManager")),
+            new Intent().setComponent(new ComponentName("com.vivo.permissionmanager",
+                    "com.vivo.permissionmanager.activity.BgStartUpManagerActivity")),
+            new Intent().setComponent(
+                    new ComponentName("com.samsung.android.lool", "com.samsung.android.sm.ui.battery.BatteryActivity")),
+            new Intent().setComponent(
+                    new ComponentName("com.htc.pitroad", "com.htc.pitroad.landingpage.activity.LandingPageActivity")),
+            new Intent()
+                    .setComponent(new ComponentName("com.asus.mobilemanager", "com.asus.mobilemanager.MainActivity")) };
+
+    @ReactMethod
+    public void hasIntent(String pkg, String cls, final Promise promise) {
+        Intent intent = buildIntentFromStrings(pkg, cls);
+        if (reactContext.getPackageManager().resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY) != null) {
+            return promise.resolve(true);
+        }
+        return promise.resolve(false);
+    }
 
     @ReactMethod
     public void startIntent(String pkg, String cls) {
-        Intent intent = new Intent();
-        ComponentName componentName = new ComponentName(pkg, cls);
-        intent.setComponent(componentName);
+        Intent intent = buildIntentFromStrings(pkg, cls);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         reactContext.startActivity(intent);
     }
 
-    @ReactMethod
-    public void isHasProtectManager(final Promise promise) {
-        for (Intent intent : POWERMANAGER_INTENTS)
-        if (reactContext.getPackageManager().resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY) != null) {
-            WritableMap map = new WritableNativeMap();
-            map.putString("package", intent.getComponent().getPackageName());
-            map.putString("class", intent.getComponent().getClassName());
-            promise.resolve(map);
-            return;
+    public Intent buildIntentFromStrings(String pkg, String cls) {
+        Intent intent = new Intent();
+        ComponentName componentName = new ComponentName(pkg, cls);
+        intent.setComponent(componentName);
+        return intent;
+    }
 
+    @ReactMethod
+    public void findPMIntent(final Promise promise) {
+        for (Intent intent : POWERMANAGER_INTENTS) {
+            if (reactContext.getPackageManager().resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY) != null) {
+                WritableMap map = new WritableNativeMap();
+                map.putString("package", intent.getComponent().getPackageName());
+                map.putString("class", intent.getComponent().getClassName());
+                return promise.resolve(map);
+            }
         }
         promise.resolve(false);
     }
